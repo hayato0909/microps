3d026418e171c893c2c059b4fa90e6d73cadd658
hayato0909 <hhamano.hayato@gmail.com>
2025-07-13 00:32:34 +0900

    step9

--- ip.c
+++ ip.c
@@ -24,10 +24,17 @@ struct ip_hdr {
     uint8_t options[];
 };
 
+struct ip_protocol {
+    struct ip_protocol *next;
+    uint8_t type;
+    void (*handler)(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface);
+};
+
 const ip_addr_t IP_ADDR_ANY = 0x00000000;
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff;
 
 static struct ip_iface *ifaces;
+static struct ip_protocol *protocols;
 
 int
 ip_addr_pton(const char *p, ip_addr_t *n)
@@ -162,6 +169,34 @@ ip_iface_select(ip_addr_t addr)
     return NULL;
 }
 
+int
+ip_protocol_register(uint8_t type, void (*handler)(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface))
+{
+    struct ip_protocol *entry;
+
+    /* Exercise 9-1 */
+    for (entry = protocols; entry; entry = entry->next) {
+        if (entry->type == type) {
+            errorf("protocol type=%u already registered", type);
+            return -1;
+        }
+    }
+
+    /* Exercise 9-2 */
+    entry = memory_alloc(sizeof(*entry));
+    if (!entry) {
+        errorf("memory_alloc() failure");
+        return -1;
+    }
+    entry->type = type;
+    entry->handler = handler;
+    entry->next = protocols;
+    protocols = entry;
+
+    infof("registered, type=%u", type);
+    return 0;
+}
+
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 {
@@ -221,6 +256,16 @@ ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 
     debugf("dev=%s, iface=%s, protocol=%u, total=%u", dev->name, ip_addr_ntop(iface->unicast, addr, sizeof(addr)), hdr->protocol, total);
     ip_dump(data, total);
+
+    /* Exercise 9-3 */
+    struct ip_protocol *proto;
+    for (proto = protocols; proto; proto = proto->next) {
+        if (proto->type == hdr->protocol) {
+            proto->handler((uint8_t *)hdr + hlen, total - hlen, hdr->src, hdr->dst, iface);
+            return;
+        }
+    }
+    /* unsupported protocol */
 }
 
 static int
--- net.c
+++ net.c
@@ -8,6 +8,7 @@
 #include "util.h"
 #include "net.h"
 #include "ip.h"
+#include "icmp.h"
 
 struct net_protocol {
     struct net_protocol *next;
@@ -260,6 +261,13 @@ net_init(void)
         errorf("ip_init() failure");
         return -1;
     }
+
+    /* Exercise 9-5 */
+    if (icmp_init() == -1) {
+        errorf("icmp_init() failure");
+        return -1;
+    }
+
     infof("initialized");
     return 0;
 }
