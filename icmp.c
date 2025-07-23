c7804effbb4bf94bb98602e46e4efb0f660d29bb
hayato0909 <hhamano.hayato@gmail.com>
2025-07-23 22:45:08 +0900

    step11

--- icmp.c
+++ icmp.c
@@ -1,9 +1,12 @@
 #include <stdint.h>
+#include <string.h>
 #include <stddef.h>
 
 #include "util.h"
 #include "ip.h"
 #include "icmp.h"
+
+#define ICMP_BUFSIZ IP_PAYLOAD_SIZE_MAX
 
 struct icmp_hdr {
     uint8_t type;
@@ -61,15 +64,15 @@ icmp_dump(const uint8_t *data, size_t len)
     fprintf(stderr, "   code: %u\n", hdr->code);
     fprintf(stderr, "   sum: 0x%04x\n", ntoh16(hdr->sum));
     switch (hdr->type) {
-        case ICMP_TYPE_ECHOREPLY:
-        case ICMP_TYPE_ECHO:
-            echo = (struct icmp_echo *)hdr;
-            fprintf(stderr, "   id: %u\n", ntoh16(echo->id));
-            fprintf(stderr, "   seq: %u\n", ntoh16(echo->seq));
-            break;
-        default:
-            fprintf(stderr, "   value: 0x%08x\n", ntoh32(hdr->values));
-            break;
+    case ICMP_TYPE_ECHOREPLY:
+    case ICMP_TYPE_ECHO:
+        echo = (struct icmp_echo *)hdr;
+        fprintf(stderr, "   id: %u\n", ntoh16(echo->id));
+        fprintf(stderr, "   seq: %u\n", ntoh16(echo->seq));
+        break;
+    default:
+        fprintf(stderr, "   value: 0x%08x\n", ntoh32(hdr->values));
+        break;
     }
 #ifdef HEXDUMP
     hexdump(stderr, data, len);
@@ -97,6 +100,48 @@ icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct
 
     debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
     icmp_dump(data, len);
+    hdr = (struct icmp_hdr *)data;
+    switch (hdr->type) {
+        case ICMP_TYPE_ECHO:
+            /* Exercise 11-3 */
+            if (icmp_output(ICMP_TYPE_ECHOREPLY, hdr->code, hdr->values, data + ICMP_HDR_SIZE, len - ICMP_HDR_SIZE, iface->unicast, src) == -1) {
+                errorf("icmp_output() failure");
+            }
+            break;
+        default:
+            /* ignore */
+            break;
+    }
+}
+
+int
+icmp_output(uint8_t type, uint8_t code, uint32_t values, const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst)
+{
+    uint8_t buf[ICMP_BUFSIZ];
+    struct icmp_hdr *hdr;
+    size_t msg_len;
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[IP_ADDR_STR_LEN];
+
+    hdr = (struct icmp_hdr *)buf;
+
+    /* Exercise 11-1 */
+    hdr->type = type;
+    hdr->code = code;
+    hdr->values = hton32(values);
+    hdr->sum = 0;
+
+    memcpy(buf + ICMP_HDR_SIZE, data, len);
+
+    msg_len = ICMP_HDR_SIZE + len;
+
+    hdr->sum = cksum16((uint16_t *)hdr, msg_len, 0);
+
+    debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(src, addr2, sizeof(addr2)), msg_len);
+    icmp_dump((uint8_t *)hdr, msg_len);
+
+    /* Exercise 11-2 */
+    return ip_output(IP_PROTOCOL_ICMP, buf, msg_len, src, dst);
 }
 
 int
